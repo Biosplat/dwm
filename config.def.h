5e3729d520664dbe442bee9d5e959c315b829d9f
Biosplat <huxonli@gmail.com>
2021-11-10 20:47:55 -0500

    font change

--- config.def.h
+++ config.def.h
@@ -7,8 +7,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
+static const char *fonts[]          = { "Iosevka Curly:size=10" };
+static const char dmenufont[]       = "Iosevka Curly:size=10";
 static const char col_gray1[]       = "#282a36";
 static const char col_gray2[]       = "#44475a";
 static const char col_gray3[]       = "#bd93f9";
@@ -61,6 +61,11 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_gray2, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char* next_cmd[] = { "playerctl", "next" };
+static const char* prev_cmd[] = { "playerctl", "previous" };
+// static const char* stop_cmd[] = { "playerctl", "pause" };
+// static const char* play_cmd[] = { "playerctl", "play" };
+
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
@@ -90,6 +95,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_minus,  setgaps,        {.i = -1 } },
 	{ MODKEY,                       XK_equal,  setgaps,        {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = 0  } },
+	{0, XF86XK_AudioNext, spawn, {.v = next_cmd}},
+	{0, XF86XK_AudioPrev, spawn, {.v = prev_cmd}},
+	// {0, XF86XK_AudioPause, spawn, {.v = stop_cmd}},
+	// {0, XF86XK_AudioPlay, spawn, {.v = play_cmd}},
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
